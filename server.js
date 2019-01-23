c1753a626ab5d4999c753565670bdfdf4a31f52a
Nikola Milev <nikola.n.milev@gmail.com>
2019-01-23 21:24:23 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log+'\n', (err)=>{
+    if(err){
+      console.log('Unable to append file.');
+    }
+    next();
+  });
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {pageTitle:'Site is under maintenance'});
+// });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+hbs.registerHelper('getCurrentYear', ()=>new Date().getFullYear());
+
+hbs.registerHelper('screamIt', text=>text.toUpperCase());
+
+app.get('/', (req, res)=>{
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome, my niggaz!',
+  });
+});
+
+app.get('/about', (req, res)=>{
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res)=>{
+  res.send({
+    errorMessage:'Unable to fullfill this request error bre'
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log('Server is up on port 3000');
+});
