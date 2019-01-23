56deaaaeabbaf8757787edd80b603e1565fe204f
Nikola Milev <nikola.n.milev@gmail.com>
2019-01-23 22:26:36 +0100

    Added the new projects page

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/bad', (req, res)=>{
   });
 });
 
+app.get('/projects', (req, res)=>{
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    message: 'Portfolio page here',
+  });
+});
+
 app.listen(port, ()=>{
   console.log(`Server is up on port ${port}`);
 });
